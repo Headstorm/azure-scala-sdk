b2370d64192c43737686bafd935c2947582e1bdb
Challen Herzberg-Brovold <challen@headstorm.com>
2020-04-11 17:57:20 -0500

    Adding storage directory

---
+++ storage/src/com/headstorm/azurescalasdk/storage/Main.scala
@@ -0,0 +1,10 @@
+package com.headstorm.azurescalasdk.storage
+
+import com.azure.storage.blob._
+import com.azure.storage.blob.models._
+
+object Main {
+
+  def main(args: Array[String]): Unit = println("Hello world")
+
+}
