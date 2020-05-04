bc15361a3454eaecfcafb746c752534f11c891a2
ericbaumann <24817191+ericbaumann@users.noreply.github.com>
2020-05-04 12:05:21 -0500

    updated Mill build script to include dependencies and plugins (wart remover, scalafmt, etc.)

--- storage/src/main/scala/com/headstorm/azure/storage/models/Models.scala
+++ storage/src/main/scala/com/headstorm/azure/storage/models/Models.scala
@@ -1,18 +1,18 @@
-package com.headstorm.azure.storage.models
+// package com.headstorm.azure.storage.models
 
-final case class Container()
+// final case class Container()
 
-final case class Blob(
-  name: String,
-  properties: BlobContainerProperties,
-  metadata: Map[String, String] = Map[String, String]()
-)
+// final case class Blob(
+//   name: String,
+//   properties: BlobContainerProperties,
+//   metadata: Map[String, String] = Map[String, String]()
+// )
 
-final case class BlobContainerProperties(
-  )
+// final case class BlobContainerProperties(
+//   )
 
-final case class File()
+// final case class File()
 
-final case class Table()
+// final case class Table()
 
-final case class Queue()
+// final case class Queue()
