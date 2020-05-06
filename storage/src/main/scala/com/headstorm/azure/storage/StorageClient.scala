ea6e0aecc8f296999ff620ee192f3c59814f9c32
ericbaumann <24817191+ericbaumann@users.noreply.github.com>
2020-05-05 21:13:09 -0400

    updated Mill build script to include dependencies and plugins (wart remover, scalafmt, etc.) (#22)

--- storage/src/main/scala/com/headstorm/azure/storage/StorageClient.scala
+++ storage/src/main/scala/com/headstorm/azure/storage/StorageClient.scala
@@ -1,62 +1,62 @@
-package com.headstorm.azure.storage
+// package com.headstorm.azure.storage
 
-import cats.effect.{ ConcurrentEffect, ContextShift, Sync, Timer }
-import cats.implicits._
-import com.headstorm.azure.storage.models.{ Blob, Container }
-import sttp.client.circe._
-import io.circe.{ Error => CirceError }
-import io.circe.generic.auto._
-import org.http4s.client.blaze.BlazeClientBuilder
-import sttp.client._
-import sttp.model.Uri
+// import cats.effect.{ ConcurrentEffect, ContextShift, Sync, Timer }
+// import cats.implicits._
+// import com.headstorm.azure.storage.models.{ Blob, Container }
+// import sttp.client.circe._
+// import io.circe.{ Error => CirceError }
+// import io.circe.generic.auto._
+// import org.http4s.client.blaze.BlazeClientBuilder
+// import sttp.client._
+// import sttp.model.Uri
 
-import scala.concurrent.ExecutionContext
+// import scala.concurrent.ExecutionContext
 
-/**
- *  Blob, Queue, Table, and File client sdk
- *
- * @tparam F Effectful type
- */
-object StorageClient {
+// /**
+//  *  Blob, Queue, Table, and File client sdk
+//  *
+//  * @tparam F Effectful type
+//  */
+// object StorageClient {
 
-  def usingClient[F[_]: ConcurrentEffect: ContextShift: Timer](
-    account: String
-  )(implicit sync: Sync[F]): F[StorageClient[F]] = {
-    import sttp.client.http4s._
-    val client = BlazeClientBuilder.apply[F](ExecutionContext.global)
-    Http4sBackend
-      .usingClientBuilder[F](client, BlockingIO.blockingContext("azure-storage-client"))
-      .use[F, StorageClient[F]](implicit backend => sync.pure(new StorageClient[F](account)))
-  }
-}
+//   def usingClient[F[_]: ConcurrentEffect: ContextShift: Timer](
+//     account: String
+//   )(implicit sync: Sync[F]): F[StorageClient[F]] = {
+//     import sttp.client.http4s._
+//     val client = BlazeClientBuilder.apply[F](ExecutionContext.global)
+//     Http4sBackend
+//       .usingClientBuilder[F](client, BlockingIO.blockingContext("azure-storage-client"))
+//       .use[F, StorageClient[F]](implicit backend => sync.pure(new StorageClient[F](account)))
+//   }
+// }
 
-class StorageClient[F[_]: ConcurrentEffect: ContextShift: Sync: Timer](account: String)(
-  implicit val backend: SttpBackend[F, fs2.Stream[F, Byte], NothingT]
-) extends AzureClient[F] {
+// class StorageClient[F[_]: ConcurrentEffect: ContextShift: Sync: Timer](account: String)(
+//   implicit val backend: SttpBackend[F, fs2.Stream[F, Byte], NothingT]
+// ) extends AzureClient[F] {
 
-  val baseURI: Uri = uri"https://$account.blob.core.windows.net"
+//   val baseURI: Uri = uri"https://$account.blob.core.windows.net"
 
-  /**
-   * API to get all containers for an account. A container contains properties, metadata, and zero or more blobs
-   *
-   * @return a List of Azure Containers
-   */
-  def listBlobContainers: F[Either[ResponseError[CirceError], Container]] =
-    basicRequest.auth.bearer(accessToken).get(uri"$baseURI/containers").response(asJson[Container]).send().map(_.body)
+//   /**
+//    * API to get all containers for an account. A container contains properties, metadata, and zero or more blobs
+//    *
+//    * @return a List of Azure Containers
+//    */
+//   def listBlobContainers: F[Either[ResponseError[CirceError], Container]] =
+//     basicRequest.auth.bearer(accessToken).get(uri"$baseURI/containers").response(asJson[Container]).send().map(_.body)
 
-  def listBlobs: F[Either[ResponseError[CirceError], Container]] =
-    basicRequest.auth.bearer(accessToken).get(uri"$baseURI/blobs").response(asJson[Container]).send().map(_.body)
+//   def listBlobs: F[Either[ResponseError[CirceError], Container]] =
+//     basicRequest.auth.bearer(accessToken).get(uri"$baseURI/blobs").response(asJson[Container]).send().map(_.body)
 
-  def updateBlob(blob: Blob): F[Either[ResponseError[CirceError], Container]] =
-    basicRequest.auth
-      .bearer(accessToken)
-      .body(blob)
-      .post(uri"$baseURI/updateblob")
-      .response(asJson[Container])
-      .send()
-      .map(_.body)
+//   def updateBlob(blob: Blob): F[Either[ResponseError[CirceError], Container]] =
+//     basicRequest.auth
+//       .bearer(accessToken)
+//       .body(blob)
+//       .post(uri"$baseURI/updateblob")
+//       .response(asJson[Container])
+//       .send()
+//       .map(_.body)
 
-  def getBlob: F[Either[ResponseError[CirceError], Container]] =
-    basicRequest.auth.bearer(accessToken).get(uri"$baseURI/getblob").response(asJson[Container]).send().map(_.body)
+//   def getBlob: F[Either[ResponseError[CirceError], Container]] =
+//     basicRequest.auth.bearer(accessToken).get(uri"$baseURI/getblob").response(asJson[Container]).send().map(_.body)
 
-}
+// }
