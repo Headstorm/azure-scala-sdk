727c766582d99bf614b582dfc6e81eb91e9f445e
nicklundin08 <nick@moontower.net>
2020-05-18 20:03:01 -0500

    Added models for list container api (#23)
    * Added models for list container api
    
    * Updated route, added request params, added content type
    
    * Fixed compilation issues
    
    * Tests pass
    
    Co-authored-by: Nick Lundin <nlundin@headstorm.com>

--- storage/src/test/scala/com/heastorm/azurescalasdk/storage/StorageClientTest.scala
+++ storage/src/test/scala/com/heastorm/azurescalasdk/storage/StorageClientTest.scala
@@ -1,15 +1,18 @@
 package com.heastorm.azurescalasdk.storage
 
-import cats.effect.IOApp
+import cats.effect.{ExitCode, IO, IOApp}
 import com.headstorm.azure.storage.StorageClient
-import org.scalatest._
+import org.scalatest.funsuite.AnyFunSuite
 
-class StorageClientTest extends FunSuite with IOApp {
 
-  val storageClient = StorageClient.usingClient("test-account")
+class StorageClientTest extends AnyFunSuite with IOApp {
+
+  val storageClient = StorageClient.usingClient[IO]("test-account")
 
   test("1 should equal 3 - 2") {
     assert(1 == 3 - 2)
   }
+
+  override def run(args: List[String]): IO[ExitCode] = {IO.never}
 
 }
